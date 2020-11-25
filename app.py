983a34fdeb52fa204375a4ce69e298e9ee6a53bb
sasha stepakov <sanechkastepakov@gmail.com>
2020-11-25 16:34:53 +0200

    second file

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-print( 'hello' )
-x = 10
-y = 20
+zoo_pet_mass = {
+    'lion': 300,
+    'skunk': 5,
+    'elephant': 5000,
+    'horse': 400
+}
+
+total_mass = 0;
+
+for animal in zoo_pet_mass.values():
+    print( animal )
+print( total_mass )
---
+++ second.py
@@ -0,0 +1,7 @@
+def some_f():
+    print( 'hey' )
+    print( 'hello' )
+
+
+some_f()
+some_f()
